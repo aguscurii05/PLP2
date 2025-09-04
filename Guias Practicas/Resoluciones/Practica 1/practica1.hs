8912c611189857f60df345614ea1fa13e09a6ccb
aguscurii05 <agustinstescovichcuri@gmail.com>
2025-09-04 13:07:34 -0300

    Termino P1

--- Guias Practicas/Resoluciones/Practica 1/practica1.hs
+++ Guias Practicas/Resoluciones/Practica 1/practica1.hs
@@ -22,7 +22,7 @@ de la lista según una función de comparación, utilizando foldr1. Por ejemplo,
 (>).
 -}
 mejorSegun:: (a -> a -> Bool) -> [a] -> a
-mejorSegun f = foldr1 (\x r->if f x r then x else r) 
+mejorSegun f = foldr1 (\x r->if f x r then x else r)
 
 {-
 iii. Definir la función sumasParciales :: Num a => [a] -> [a], que dada una lista de números devuelve
@@ -183,7 +183,7 @@ sumaMat :: [[Int]] -> [[Int]] -> [[Int]]
 -}
 
 sumaMat :: [[Int]] -> [[Int]] -> [[Int]]
-sumaMat  = zipWith (zipWith (+)) 
+sumaMat  = zipWith (zipWith (+))
 
 {-
 ii. Escribir la función trasponer, que, dada una matriz como las del ítem i, devuelva su traspuesta. Es decir,
@@ -192,7 +192,7 @@ entrada es una lista de N listas, todas de longitud M , la salida debe tener M l
 trasponer :: [[Int]] -> [[Int]]
 -}
 nesimo :: Int -> [Int] -> Int
-nesimo n xs = foldr (\x r k -> if k == 0 then x else r (k-1)) (const 0) xs n 
+nesimo n xs = foldr (\x r k -> if k == 0 then x else r (k-1)) (const 0) xs n
 
 transponer::[[Int]]->[[Int]]
 transponer mat = [concatMap ((:[]).nesimo n) mat|n<-[0..length (head mat)-1]]
@@ -225,8 +225,8 @@ de la lista. Dicha función de incremento, dado un elemento de la lista, devuelv
 
 
 genLista :: a -> (a -> a) -> Int -> [a]
-genLista ini f 0 = [] 
-genLista ini f cant = ini:genLista (f ini) f (cant-1) 
+genLista ini f 0 = []
+genLista ini f cant = ini:genLista (f ini) f (cant-1)
 
 --genLista 'a' (\x->chr (ord x+2)) 3
 
@@ -322,3 +322,232 @@ esABB = recAB (\r i d ri rd->caso (>=) r i&& caso (<) r d&& ri && rd) True
     where caso f r ab= case mejorSegunAB f ab of
             Nothing->True
             (Just x)->f r x
+
+
+{-
+Ejercicio 13
+Dado el tipo AB a del ejercicio 12:
+
+i. Definir las funciones ramas (caminos desde la raíz hasta las hojas), cantHojas y espejo.
+
+ii. Definir la función mismaEstructura :: AB a -> AB b -> Bool que, dados dos árboles, indica si éstos
+tienen la misma forma, independientemente del contenido de sus nodos. Pista: usar evaluación parcial y
+recordar el ejercicio 7.
+-}
+
+ramas::AB a->[[a]]
+ramas = recAB (\r i d ri rd->case (i,d) of
+    (Nil,Nil)->[[r]]
+    (Nil,_)->addRaiz r rd
+    (_,Nil)->addRaiz r ri
+    (_,_)->addRaiz r ri++addRaiz r rd) [[]]
+    where addRaiz r= map (r:)
+arbol = Bin (Bin (Bin Nil 1 Nil) 4 (Bin Nil 7 Nil)) 6 (Bin (Bin Nil 3 Nil) 2 (Bin Nil 9 Nil))
+arbol' = Bin (Bin (Bin Nil 7 Nil) 4 (Bin Nil 9 Nil)) 6 (Bin (Bin Nil 5 Nil) 2 Nil)
+arbol2 = Bin Nil 3 Nil
+{-
+            6
+           /  \
+          4     2
+         / \    / \ 
+        1   7   3  9
+
+
+-}
+cantHojas::AB a->Int
+cantHojas = recAB (\r i d ri rd->if esNil i && esNil d then 1 else ri+rd) 0
+
+espejo::AB a->AB a
+espejo = foldAB (\ri r rd->Bin rd r ri) Nil
+
+
+mismaEstructura :: AB a -> AB b -> Bool
+mismaEstructura = foldAB (\ri r rd ab->case ab of
+    Nil->False
+    (Bin i r d)->ri i && rd d) esNil
+
+
+--EJERCICIO 14
+
+data AIH a = Hoja a | BinH (AIH a) (AIH a)
+
+arbolAIH = BinH (BinH (Hoja 1) (Hoja 2)) (BinH (BinH (Hoja 3) (Hoja 4)) (BinH (Hoja 5) (BinH (Hoja 6) (Hoja 7))))
+{-
+          /         \
+         / \        /   \  
+        1  2       /\   /   \
+                  3  4  5   /\
+                           6  7
+-}
+{-
+a) Definir el esquema de recursión estructural foldAIH y dar su tipo. Por tratarse del primer esquema de
+recursión que tenemos para este tipo, se permite usar recursión explícita.
+-}
+foldAIH::(b->b->b)->(a->b)->AIH a->b
+foldAIH cBin cHoja (Hoja a) = cHoja a
+foldAIH cBin cHoja (BinH i d) = cBin (rec i) (rec d)
+    where rec = foldAIH cBin cHoja
+
+{-
+b) Escribir las funciones altura :: AIH a -> Integer y tamaño :: AIH a -> Integer.
+Considerar que la altura de una hoja es 1 y el tamaño de un AIH es su cantidad de hojas.
+-}
+alturaAIH::AIH a->Integer
+alturaAIH = foldAIH (\ri rd->1+max rd ri) (const 1)
+
+tamaño::AIH a->Integer
+tamaño = foldAIH (+) (const 1)
+
+{-
+Ejercicio 15 ⋆
+i. Definir el tipo RoseTree de árboles no vacíos, con una cantidad indeterminada de hijos para cada nodo.
+
+ii. Escribir el esquema de recursión estructural para RoseTree. Importante escribir primero su tipo.
+
+iii. Usando el esquema definido, escribir las siguientes funciones:
+
+    a) hojas, que dado un RoseTree, devuelva una lista con sus hojas ordenadas de izquierda a derecha,
+    según su aparición en el RoseTree.
+    b) distancias, que dado un RoseTree, devuelva las distancias de su raíz a cada una de sus hojas.
+    c) altura, que devuelve la altura de un RoseTree (la cantidad de nodos de la rama más larga). Si el
+    RoseTree es una hoja, se considera que su altura es 1.
+-}
+data RoseTree a = Rose a [RoseTree a]
+
+rt = Rose 5 [Rose 4 [],
+             Rose 6 [],
+             Rose 7 [Rose 6 [],Rose 9 [Rose 8 []]]]
+{-
+          5
+       4 6   7
+            6 9
+               8   
+-}
+foldRose :: (a -> [b] -> b) -> RoseTree a -> b
+foldRose f (Rose a xs) = f a (map (foldRose f) xs)
+
+hojas::RoseTree a->[a]
+hojas = foldRose (\r rs->if null rs then [r] else concat rs)
+
+distancias::RoseTree a->[Int]
+distancias = foldRose (\r rs->if null rs then [0] else map (1+) (concat rs))
+
+alturaRose::RoseTree a->Int
+alturaRose = foldRose (\r rs->if null rs then 1 else 1+ maximum rs)
+
+{-
+EJERCICIO 16 (Opcional)
+
+Se desea representar conjuntos mediante Hashing abierto (chain addressing). El Hashing abierto consta de
+dos funciones: una función de Hash, que dado un elemento devuelve un valor entero (el cual se espera que no se
+repita con frecuencia), y una tabla de Hash, que dado un número entero devuelve los elementos del conjunto a
+los que la función de Hash asignó dicho número (es decir, la preimagen de la función de Hash para ese número).
+Los representaremos en Haskell de la siguiente manera:-}
+data HashSet a = Hash (a -> Integer) (Integer -> [a])
+{-Por contexto de uso, vamos a suponer que la tabla de Hash es una función total, que devuelve listas vacías
+para los números que no corresponden a elementos del conjunto. Este es un invariante que deberá preservarse
+en todas las funciones que devuelvan conjuntos.
+Definir las siguientes funciones:-}
+
+{-
+i. vacío :: (a -> Integer) -> HashSet a, que devuelve un conjunto vacío con la función de Hash indica-
+da.-}
+vacio :: (a -> Integer) -> HashSet a
+vacio f = Hash f (const [])
+
+
+{-ii. pertenece :: Eq a => a -> HashSet a -> Bool, que indica si un elemento pertenece a un conjunto. Es
+decir, si se encuentra en la lista obtenida en la tabla de Hash para el número correspondiente a la función
+de Hash del elemento.
+Por ejemplo:
+pertenece 5 $ agregar 1 $ agregar 2 $ agregar 1 $ vacío (flip mod 5) devuelve False.
+pertenece 2 $ agregar 1 $ agregar 2 $ agregar 1 $ vacío (flip mod 5) devuelve True.-}
+
+pertenece :: Eq a => a -> HashSet a -> Bool
+pertenece e (Hash func tab) = (not.null) (tab num)
+    where num = func e
+
+{-iii. agregar :: Eq a => a -> HashSet a -> HashSet a, que agrega un elemento a un conjunto. Si el ele-
+mento ya estaba en el conjunto, se debe devolver el conjunto sin modificaciones.-}
+
+agregar :: Eq a => a -> HashSet a -> HashSet a
+agregar e (Hash func tab) | pertenece e (Hash func tab) = Hash func tab
+                          | otherwise = Hash func (\n-> if n ==func e then e:tab n else tab n)
+
+
+{-iv. intersección :: Eq a => HashSet a -> HashSet a -> HashSet a que, dados dos conjuntos, devuel-
+ve un conjunto con la misma función de Hash del primero y con los elementos que pertenecen a ambos
+conjuntos a la vez.-}
+
+interseccion :: Eq a => HashSet a -> HashSet a -> HashSet a
+interseccion (Hash f1 t1) (Hash f2 t2) = Hash f1 (\n-> int (t1 n) (t2 n))
+    where int xs ys = foldr (\x rec-> if elem x ys then x:rec else rec) [] xs
+
+
+{-v. foldr1(no relacionada con los conjuntos). Dar el tipo y definir la función foldr1 para listas sin usar
+recursión explícita, recurriendo a alguno de los esquemas de recursión conocidos.
+Se recomienda usar la función error :: String -> a para el caso de la lista vacía.
+-}
+h1 = agregar 6 $ agregar 5 $ agregar 2 $ agregar 1 $ vacio (`mod` 5)
+h2 = agregar 1 $ agregar 5 $ agregar 2 $ agregar 3 $ vacio (`mod` 5)
+
+h3 = interseccion h1 h2
+
+{-
+EJERCICIO 17
+
+¿Cuál es el valor de esta expresión?
+[ x | x <- [1..3], y <- [x..3], (x + y) `mod' 3 == 0 ]
+
+(M) = [1,2]
+Rta = [1,3]
+
+Tiene sentido... mod (1+2) 3 = 0 y mod (3+3) 3 =0 pero mod (2+2) y (2+3) \= 0
+-}
+lst = [ x | x <- [1..3], y <- [x..3], mod (x+y) 3 == 0 ]
+
+{-
+EJERCICIO 18 ⋆
+Definir la lista infinita paresDeNat::[(Int,Int)], que contenga todos los pares de números naturales: (0,0),
+(0,1), (1,0), etc.
+-}
+paresDeNat::[(Int,Int)]
+paresDeNat = [(x,y) |n<-[0..], x<-[0..n],y<-[0..n],x+y==n ]
+
+{-
+EJERCICIO 19
+
+Ejercicio 19
+Una tripla pitagórica es una tripla (a, b, c) de enteros positivos tal que a2 + b2 = c2.
+La siguiente expresión intenta ser una definición de una lista (infinita) de triplas pitagóricas:
+pitagóricas :: [(Integer, Integer, Integer)]
+pitagóricas = [(a, b, c) | a <- [1..], b <-[1..], c <- [1..], a^2 + b^2 == c^2]
+Explicar por qué esta definición no es útil. Dar una definición mejor.
+
+No es util porque primero de todo, busca para a=1 y b=1 alguna tripla pitagorica. Como nunca termina, no muestra nunca nada pues va a probar si existe alguna tripla de la forma (1,1,c) (i.e 2=c^2)
+-}
+
+pitagoricas :: [(Integer, Integer, Integer)]
+pitagoricas = [(a, b, c) |n<-[1..], a<-[1..n],b<-[1..n],c<-[1..n] , a^2 + b^2 == c^2 && a+b+c==n]
+
+{-
+Ejercicio 20 ⋆
+Escribir la función listasQueSuman :: Int -> [[Int]] que, dado un número natural n, devuelve todas las
+listas de enteros positivos (es decir, mayores o iguales que 1) cuya suma sea n. Para este ejercicio se permite
+usar recursión explícita. Pensar por qué la recursón utilizada no es estructural. (Este ejercicio no es de
+generación infinita, pero puede ser útil para otras funciones que generen listas infinitas de listas).
+-}
+
+
+
+listasQueSuman ::Int -> [[Int]]
+listasQueSuman 0 = [[]]
+listasQueSuman n = [x:xs | x<-[1..n],xs<-listasQueSuman (n-x)]
+
+{-
+Ejercicio 21 ⋆
+Definir en Haskell una lista que contenga todas las listas finitas de enteros positivos (esto es, con elementos
+mayores o iguales que 1).
+-}
+
+listas = [xs |n<-[1..],xs<-listasQueSuman n]
